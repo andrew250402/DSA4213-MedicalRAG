77a1d99a5a07a2b6185b43ea24cd95abfadef1d4
andrew250402 <n.huydat2504@gmail.com>
2025-11-16 20:21:31 +0800

    Did knowledge base experiment

--- src/config.py
+++ src/config.py
@@ -17,8 +17,8 @@ RETRIEVER_TYPE = "faiss"   # Options: "faiss" or "bm25"
 MODEL = "gpt-3.5-turbo" # Options: "gpt-3.5-turbo" or "gpt-4o-mini"
 
 # Paths to vector stores
-FAISS_PATH = "store_v2/faiss/openai-text-embedding-3-small"
-BM25_PATH = "store_v2/bm25"
+FAISS_PATH = "store/faiss/openai-text-embedding-3-small"
+BM25_PATH = "store/bm25"
 
 # Prompt style
 PROMPT_STYLE = "plain"  # Options: "plain", "citation", "evidence", "safety", "fewshot", "gold style"
